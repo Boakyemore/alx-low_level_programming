ba658c5218bde8b8390502b47b05efc5ffd46b92
Boakyemore <122833465+Boakyemore@users.noreply.github.com>
2023-03-15 04:35:54 -0700

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,45 +1,15 @@
-#include "main.h"
-		
-#include <stdlib.h>
-		
+#include "bootcamp.h"
 /**
-		
- * create_array - create array of size size and assign char c
-		
- * @size: size of array
-		
- * @c: char to assign
-		
- * Description: creat array of size size and assign char c
-		
- * Return: pointer to array, NULL if fail
-		
- *
-		
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int of amount of needed memory
+ * Return: void
  */
-		
-char *create_array(unsigned int size, char c)
-		
+void *malloc_checked(unsigned int b)
 {
-		
-	char *str;
-		
-	unsigned int i;
-		
+	void *p;
 
-		
-	str = malloc(sizeof(char) * size);
-		
-	if (size == 0 || str == NULL)
-		
-		return (NULL);
-		
-
-		
-	for (i = 0; i < size; i++)
-		
-		str[i] = c;
-		
-	return (str);
-		
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
